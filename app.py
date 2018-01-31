908c817ffb145f7e46e69ca18e77326bb436c3f6
DaniilBelov <daniilbelov98@yandex.ru>
2018-01-31 20:00:55 +0300

    Cron

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', hours=4, id='scanRightmove')
+@sched.scheduled_job('cron', hour=23, id='scanRightmove')
 def scanRightmove():
     headers = {'User-Agent': 'Dan070Bot(daniilbelov98@yandex.ru)'}
     url = "http://www.rightmove.co.uk/property-to-rent/find.html?locationIdentifier=USERDEFINEDAREA%5E%7B%22id%22%3A4703045%7D&maxPrice=800&savedSearchId=25639538&minBedrooms=2&retirement=false&letFurnishType=furnished"
@@ -25,10 +25,6 @@ def scanRightmove():
 
     data = soup.select('div.l-searchResult.is-list')
 
-    with open(os.path.join(__location__, 'got.json')) as data_file:
-        old_links = json.load(data_file)
-
-    links_today = []
     t_url = 'https://api.telegram.org/bot538125304:AAEodL7ns7iuTbbpRPgpLteOs-o4UAunV6k/sendMessage'
     headers = {'Content-Type': 'application/json'}
 
@@ -38,24 +34,10 @@ def scanRightmove():
 
         if added == 'Added today' or added == 'Reduced today':
             link = property.select('a.propertyCard-link')[0].get('href')
-            full_link = 'http://www.rightmove.co.uk' + link
 
-            if full_link not in old_links:
-                links_today.append(full_link)
-                payload['text'] = full_link
-                requests.post(t_url, data=json.dumps(payload), headers=headers);
-
-    old_links = old_links + links_today
-
-    with open(os.path.join(__location__, 'got.json'), 'w') as outfile:
-        json.dump(old_links, outfile)
+            payload['text'] = full_link = 'http://www.rightmove.co.uk' + link
+            requests.post(t_url, data=json.dumps(payload), headers=headers);
 
     print('Cycle')
-
-
-@sched.scheduled_job('cron', minute=0, hour=0, id='clearOldLinks')
-def clearoldLinks():
-    with open(os.path.join(__location__, 'got.json'), 'w') as outfile:
-        json.dump([], outfile)
 
 sched.start()
